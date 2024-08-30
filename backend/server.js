ddab66b8b719982fbec2931b4838031879f61b5c
Freeman-Adelaide <freemanijale@gmail.com>
2024-08-30 01:53:09 +0100

    3- payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //middleware
-app.use(express.json()) // to parse req.body
+app.use(express.json({limit: "5mb"})) // to parse req.body
+//limit shouldn't be too high to prevent DOS attack
 app.use(cors())
 app.use(express.urlencoded({ extended: true })); //to parse form data
 app.use(cookieParser());
