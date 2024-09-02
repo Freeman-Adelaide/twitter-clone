4d821de9ead15be872b21190cd8a6be3e0e7f96e
Freeman-Adelaide <freemanijale@gmail.com>
2024-09-03 00:49:27 +0100

    3- Profile page posts and user info fetched

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -9,7 +9,7 @@ const getUserProfile = async (req, res) => {
     const { username } = req.params;
 
     try {
-        const user = User.findOne({username}).select("-password");
+        const user = await User.findOne({username}).select("-password");
         if(!user) return res.status(404).json({error: "User not found"});
         res.status(200).json(user);
 
