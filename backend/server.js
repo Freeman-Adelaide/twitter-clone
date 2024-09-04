d24369d7dd081499d331b7dda73bda2a1c88df90
Freeman-Adelaide <freemanijale@gmail.com>
2024-09-04 09:22:04 +0100

    5- corrected the server code for redeployment

--- backend/server.js
+++ backend/server.js
@@ -22,14 +22,14 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve
+const __dirname = path.resolve();  
 
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    })
+    });
 }
 
 //middleware
