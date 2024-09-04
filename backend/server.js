3f3a731f0a526b3f30f4a25084830c8daf4ed28e
Freeman-Adelaide <freemanijale@gmail.com>
2024-09-04 08:51:25 +0100

    4- prepared our app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-import express from 'express'
+import path from "path";
+import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
@@ -21,6 +22,15 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 //middleware
 app.use(express.json({limit: "5mb"})) // to parse req.body
